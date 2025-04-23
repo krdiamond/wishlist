d399cf9f851dc9c2f796c7b2cfaab9b0c2873031
Kristina Diamond <krdiamond@gmail.com>
2025-04-23 19:28:19 -0400

    added server

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # wishlist
+
+ http://127.0.0.1:8080
+
+ to run server: http-server
--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function showAlert() {
     alert('Hello from JavaScript!');
   } 
+
+  
+  // Load JSON data
+  fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+
+    })
+    .catch(error => {
+      console.error("Failed to load JSON:", error);
+    });
+  
